b9886b4577d2c84ff6b9e1544996a6e6a4114a49
pratiksha-dighe123 <dpratiksha292@gmail.com>
2026-01-02 14:44:04 +0530

    Added Binary to decimal number conversion program

---
+++ src/number_system/BinToDecimal.java
@@ -0,0 +1,25 @@
+package number_system;
+
+public class BinToDecimal {
+
+    public int conversion(int binNum){
+        int ans=0;
+        int pow=1;
+
+        while(binNum > 0){
+            int rem= binNum % 10;
+            ans += rem * pow;
+            binNum= binNum/10;
+            pow = pow *2;
+        }
+        return ans;
+    }
+
+    public static void main(String[] args){
+        int binNum;
+        BinToDecimal bd = new BinToDecimal();
+        System.out.println(bd.conversion(1100101));
+    }
+
+
+}
--- src/number_system/DecToBin.java
+++ src/number_system/DecToBin.java
@@ -2,10 +2,11 @@ package number_system;
 
 public class DecToBin {
 
-    int ans = 0;
-    int pow = 1;
-
     public int conversion(int decNum) {
+
+        int ans = 0;
+        int pow = 1;
+
         while (decNum > 0) {
             int rem = decNum % 2;
             decNum = decNum / 2;
@@ -19,7 +20,11 @@ public class DecToBin {
     public static void main(String[] args){
         int decNum;
         DecToBin db = new DecToBin();
-        System.out.println("The binary no is" +db.conversion(50));
+//        System.out.println("The binary no is: " +db.conversion(50));
+
+        for(int i=1; i<=10 ; i++){
+            System.out.println(i +"->" +db.conversion(i));
+        }
     }
 
 }

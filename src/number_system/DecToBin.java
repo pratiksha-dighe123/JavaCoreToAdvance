a1761f266cf06edf041eeea1f1c6ce6f74a87fcd
pratiksha-dighe123 <dpratiksha292@gmail.com>
2026-01-02 08:18:15 +0530

    Added Decimal to binary number conversion program

---
+++ src/number_system/DecToBin.java
@@ -0,0 +1,25 @@
+package number_system;
+
+public class DecToBin {
+
+    int ans = 0;
+    int pow = 1;
+
+    public int conversion(int decNum) {
+        while (decNum > 0) {
+            int rem = decNum % 2;
+            decNum = decNum / 2;
+
+            ans += rem * pow;
+            pow = pow * 10;
+        }
+        return ans;
+    }
+
+    public static void main(String[] args){
+        int decNum;
+        DecToBin db = new DecToBin();
+        System.out.println(db.conversion(50));
+    }
+
+}

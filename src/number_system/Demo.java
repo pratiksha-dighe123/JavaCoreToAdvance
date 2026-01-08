da7a42b12bcadcb09fc63ba851697886928c5c24
pratiksha-dighe123 <dpratiksha292@gmail.com>
2026-01-08 12:44:29 +0530

    String Practice code

---
+++ src/number_system/Demo.java
@@ -0,0 +1,10 @@
+package number_system;
+
+public class Demo {
+    public static void main() {
+        String str = "abc";
+        System.out.println(str);
+        str += "efg";
+        System.out.println(str);
+    }
+}
